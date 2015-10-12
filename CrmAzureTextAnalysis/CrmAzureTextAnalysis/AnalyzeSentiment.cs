df82968fa7dd002f32eaa7389c61c61137710e88
Lucas Alexander <lucas@alexanderdevelopment.net>
2015-10-12 14:43:30 -0500

    initial commit of Azure sentiment analysis solution

---
+++ CrmAzureTextAnalysis/CrmAzureTextAnalysis/AnalyzeSentiment.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Activities;
+using System.ServiceModel;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Runtime.Serialization;
+
+namespace CrmAzureTextAnalysis
+{
+    /// <summary>
+    /// This is a custom workflow activity for performing sentiment analysis using Azure text analytics API:
+    ///  - https://datamarket.azure.com/dataset/amla/text-analytics
+    ///  - http://blogs.technet.com/b/machinelearning/archive/2015/04/08/introducing-text-analytics-in-the-azure-ml-marketplace.aspx
+    ///  - https://azure.microsoft.com/en-us/documentation/articles/machine-learning-apps-text-analytics/
+    /// 
+    /// This is based on code I originally wrote for performing sentiment analysis using HP IDOL OnDemand (now called HP Haven OnDemand)
+    ///  - https://github.com/lucasalexander/CRM-IdolOnDemand-Tools
+    ///  - http://h30507.www3.hp.com/t5/Applications-Services-Blog/Using-IDOL-OnDemand-for-text-analysis-in-Dynamics-CRM-Part-1/ba-p/171242
+    ///  - http://h30507.www3.hp.com/t5/Applications-Services-Blog/Using-IDOL-OnDemand-for-text-analysis-in-Dynamics-CRM-Part-2/ba-p/171424
+    /// </summary>
+    public sealed class AnalyzeSentiment : CodeActivity
+    {
+        /// <summary>
+        /// text to analyze
+        /// </summary>
+        [RequiredArgument]
+        [Input("Text Input")]
+        public InArgument<String> TextInput { get; set; }
+
+        /// <summary>
+        /// azure text analytics access key 
+        /// </summary>
+        [RequiredArgument]
+        [Input("API Key")]
+        public InArgument<String> ApiKey { get; set; }
+
+        /// <summary>
+        /// sentiment score - numbers closer to 1 are more positive, numbers closer to 0 are more negative
+        /// </summary>
+        [Output("Score")]
+        public OutArgument<Decimal> Score { get; set; }
+
+        //web service address
+        private string _webAddress = "https://api.datamarket.azure.com/data.ashx/amla/text-analytics/v1/GetSentimentBatch";
+
+        //name of your custom workflow activity for tracing/error logging
+        private string _activityName = "AnalyzeSentiment";
+
+        /// <summary>
+        /// Executes the workflow activity.
+        /// </summary>
+        /// <param name="executionContext">The execution context.</param>
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            // Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            if (tracingService == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve tracing service.");
+            }
+
+            tracingService.Trace("Entered " + _activityName + ".Execute(), Activity Instance Id: {0}, Workflow Instance Id: {1}",
+                executionContext.ActivityInstanceId,
+                executionContext.WorkflowInstanceId);
+
+            // Create the context
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+
+            if (context == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve workflow context.");
+            }
+
+            tracingService.Trace(_activityName + ".Execute(), Correlation Id: {0}, Initiating User: {1}",
+                context.CorrelationId,
+                context.InitiatingUserId);
+
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                string inputText = TextInput.Get(executionContext);
+                if (inputText != string.Empty)
+                {
+                    //strip any html characters from the text to analyze
+                    inputText = HtmlTools.StripHTML(inputText);
+                    tracingService.Trace(string.Format("stripped text: {0}", inputText));
+
+                    //escape any double quote characters (") so they don't cause problems when posting the json later
+                    string escapedText = inputText.Replace("\"", "\\\"");
+                    tracingService.Trace(string.Format("escaped text: {0}", escapedText));
+
+                    //create the webrequest object
+                    HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(_webAddress);
+
+                    //set request content type so it is treated as a regular form post
+                    req.ContentType = "application/x-www-form-urlencoded";
+
+                    //set method to post
+                    req.Method = "POST";
+
+                    //buld the request
+                    StringBuilder postData = new StringBuilder();
+
+                    //note the Id value set to 1 - we can hardcode this because we're only sending a batch of one
+                    postData.Append("{\"Inputs\":[{\"Id\":\"1\",\"Text\":\""+escapedText+"\"}]}");
+
+                    //set the authentication using the azure service access key
+                    string authInfo = "AccountKey:" + ApiKey.Get(executionContext);
+                    authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+                    req.Headers["Authorization"] = "Basic " + authInfo;
+
+                    //create a stream
+                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes(postData.ToString());
+                    req.ContentLength = bytes.Length;
+                    System.IO.Stream os = req.GetRequestStream();
+                    os.Write(bytes, 0, bytes.Length);
+                    os.Close();
+
+                    //post the request and get the response
+                    System.Net.WebResponse resp = req.GetResponse();
+
+                    //deserialize the response to a SentimentBatchResult object
+                    SentimentBatchResult sentimentResult = new SentimentBatchResult();
+                    System.Runtime.Serialization.Json.DataContractJsonSerializer deserializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(sentimentResult.GetType());
+                    sentimentResult = deserializer.ReadObject(resp.GetResponseStream()) as SentimentBatchResult;
+
+                    //if no errors, return the sentiment
+                    if (sentimentResult.Errors.Count == 0)
+                    {
+                        //set output values from the fields of the deserialzed myjsonresponse object
+                        Score.Set(executionContext, sentimentResult.SentimentBatch[0].Score);
+                    }
+                    else
+                    {
+                        //otherwise raise an exception with the returned error message
+                        throw new InvalidPluginExecutionException(String.Format("Sentiment analyis error: {0)", sentimentResult.Errors[0].Message));
+                    }
+                }
+            }
+
+            catch (WebException exception)
+            {
+                string str = string.Empty;
+                if (exception.Response != null)
+                {
+                    using (StreamReader reader =
+                        new StreamReader(exception.Response.GetResponseStream()))
+                    {
+                        str = reader.ReadToEnd();
+                    }
+                    exception.Response.Close();
+                }
+                if (exception.Status == WebExceptionStatus.Timeout)
+                {
+                    throw new InvalidPluginExecutionException(
+                        "The timeout elapsed while attempting to issue the request.", exception);
+                }
+                throw new InvalidPluginExecutionException(String.Format(CultureInfo.InvariantCulture,
+                    "A Web exception ocurred while attempting to issue the request. {0}: {1}",
+                    exception.Message, str), exception);
+            }
+            catch (FaultException<OrganizationServiceFault> e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+
+                // Handle the exception.
+                throw;
+            }
+            catch (Exception e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+                throw;
+            }
+
+            tracingService.Trace("Exiting " + _activityName + ".Execute(), Correlation Id: {0}", context.CorrelationId);
+        }
+
+    }
+
+    /// <summary>
+    /// class to represent invidual sentiment result item
+    /// </summary>
+    [DataContract]
+    public class SentimentBatchResultItem
+    {
+        [DataMember(Name = "Score")]
+        public Decimal Score { get; set; }
+
+        [DataMember(Name = "Id")]
+        public String Id { get; set; }
+    }
+
+    /// <summary>
+    /// class to represent invidual error item
+    /// </summary>
+    [DataContract]
+    public class ErrorRecord
+    {
+        [DataMember(Name = "Id")]
+        public String Id { get; set; }
+
+        [DataMember(Name = "Message")]
+        public String Message { get; set; }
+    }
+
+    /// <summary>
+    /// class to represent sentiment result batch response
+    /// </summary>
+    [DataContract]
+    public class SentimentBatchResult
+    {
+        [DataMember(Name = "SentimentBatch")]
+        public List<SentimentBatchResultItem> SentimentBatch { get; set; }
+
+        [DataMember(Name = "Errors")]
+        public List<ErrorRecord> Errors { get; set; }
+    }
+}
---
+++ CrmAzureTextAnalysis/CrmAzureTextAnalysis/HtmlTools.cs
@@ -0,0 +1,185 @@
+﻿namespace CrmAzureTextAnalysis
+{
+    class HtmlTools
+    {
+        //http://www.codeproject.com/Articles/11902/Convert-HTML-to-Plain-Text
+        public static string StripHTML(string source)
+        {
+            try
+            {
+                string result;
+
+                // Remove HTML Development formatting
+                // Replace line breaks with space
+                // because browsers inserts space
+                result = source.Replace("\r", " ");
+                // Replace line breaks with space
+                // because browsers inserts space
+                result = result.Replace("\n", " ");
+                // Remove step-formatting
+                result = result.Replace("\t", string.Empty);
+                // Remove repeating spaces because browsers ignore them
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                                                                      @"( )+", " ");
+
+                // Remove the header (prepare first by clearing attributes)
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*head([^>])*>", "<head>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"(<( )*(/)( )*head( )*>)", "</head>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(<head>).*(</head>)", string.Empty,
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // remove all scripts (prepare first by clearing attributes)
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*script([^>])*>", "<script>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"(<( )*(/)( )*script( )*>)", "</script>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                //result = System.Text.RegularExpressions.Regex.Replace(result,
+                //         @"(<script>)([^(<script>\.</script>)])*(</script>)",
+                //         string.Empty,
+                //         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"(<script>).*(</script>)", string.Empty,
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // remove all styles (prepare first by clearing attributes)
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*style([^>])*>", "<style>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"(<( )*(/)( )*style( )*>)", "</style>",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(<style>).*(</style>)", string.Empty,
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // insert tabs in spaces of <td> tags
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*td([^>])*>", "\t",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // insert line breaks in places of <BR> and <LI> tags
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*br( )*>", "\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*li( )*>", "\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // insert line paragraphs (double line breaks) in place
+                // if <P>, <DIV> and <TR> tags
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*div([^>])*>", "\r\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*tr([^>])*>", "\r\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<( )*p([^>])*>", "\r\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // Remove remaining tags like <a>, links, images,
+                // comments etc - anything that's enclosed inside < >
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"<[^>]*>", string.Empty,
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // replace special characters:
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @" ", " ",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&bull;", " * ",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&lsaquo;", "<",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&rsaquo;", ">",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&trade;", "(tm)",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&frasl;", "/",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&lt;", "<",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&gt;", ">",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&copy;", "(c)",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&reg;", "(r)",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                // Remove all others. More can be added, see
+                // http://hotwired.lycos.com/webmonkey/reference/special_characters/
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         @"&(.{2,6});", string.Empty,
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // for testing
+                //System.Text.RegularExpressions.Regex.Replace(result,
+                //       this.txtRegex.Text,string.Empty,
+                //       System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // make line breaking consistent
+                result = result.Replace("\n", "\r");
+
+                // Remove extra line breaks and tabs:
+                // replace over 2 breaks with 2 and over 4 tabs with 4.
+                // Prepare first to remove any whitespaces in between
+                // the escaped characters and remove redundant tabs in between line breaks
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\r)( )+(\r)", "\r\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\t)( )+(\t)", "\t\t",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\t)( )+(\r)", "\t\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\r)( )+(\t)", "\r\t",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                // Remove redundant tabs
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\r)(\t)+(\r)", "\r\r",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                // Remove multiple tabs following a line break with just one tab
+                result = System.Text.RegularExpressions.Regex.Replace(result,
+                         "(\r)(\t)+", "\r\t",
+                         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                // Initial replacement target string for line breaks
+                string breaks = "\r\r\r";
+                // Initial replacement target string for tabs
+                string tabs = "\t\t\t\t\t";
+                for (int index = 0; index < result.Length; index++)
+                {
+                    result = result.Replace(breaks, "\r\r");
+                    result = result.Replace(tabs, "\t\t\t\t");
+                    breaks = breaks + "\r";
+                    tabs = tabs + "\t";
+                }
+
+                // That's it.
+                return result;
+            }
+            catch
+            {
+                return source;
+            }
+        }
+    }
+
+}
